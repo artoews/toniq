3329789d001a0ea9cb5e41fc3bc338c4a6b7eafa
Alexander R Toews <artoews@stanford.edu>
2023-09-11 17:25:12 -0700

    add interpolation, FWHM; use more sigpy functions

--- psf.py
+++ psf.py
@@ -3,21 +3,16 @@ import sigpy as sp
 
 
 def model(image, psf_size):
-    Z = pad_psf_op(image.shape, psf_size)
+    Z = sp.linop.Resize(image.shape, (psf_size,) * image.ndim)
     F = sp.linop.FFT(image.shape)
     D = sp.linop.Multiply(image.shape, sp.fft(image))
-    C = crop_wrap_op(image.shape, psf_size)
+    C = sp.linop.Resize(shape_without_margin(image.shape, psf_size), image.shape)
     return C * F.H * D * F * Z
 
-def crop_wrap_op(ishape, psf_size):
-    idx = tuple(slice(psf_size // 2, n - psf_size // 2) for n in ishape)
-    return sp.linop.Slice(ishape, idx)
+def shape_without_margin(shape, margin):
+    return tuple(n - margin for n in shape)
 
-def pad_psf_op(shape, psf_size):
-    idx = tuple(slice(n // 2 - psf_size // 2, n // 2 + psf_size // 2) for n in shape)
-    return sp.linop.Embed(shape, idx)
-
-def estimate_psf(image_ref, image_blur, patch_size, psf_size, l2=1e-5):
+def estimate_psf(image_ref, image_blur, patch_size, psf_size):
     start = (50, 70, 30)
     ndim = image_ref.ndim
     patch = tuple(slice(start[i], start[i] + patch_size + psf_size) for i in range(ndim))
@@ -25,9 +20,24 @@ def estimate_psf(image_ref, image_blur, patch_size, psf_size, l2=1e-5):
     # init[patch_size//2, patch_size//2, patch_size//2] = 1
     image_ref_patch = image_ref[patch]
     image_blur_patch = image_blur[patch]
-    C = crop_wrap_op(image_blur_patch.shape, psf_size)
     op = model(image_ref_patch, psf_size)
-    app = sp.app.LinearLeastSquares(op, C(image_blur_patch), x=psf_init.copy(), lamda=l2)
+    image_ref_patch = sp.util.resize(image_ref_patch, shape_without_margin(image_ref_patch.shape, psf_size))
+    image_blur_patch = sp.util.resize(image_blur_patch, shape_without_margin(image_blur_patch.shape, psf_size))
+    app = sp.app.LinearLeastSquares(op, image_blur_patch, x=psf_init.copy(), tol=1e-4, max_iter=1e3)
     psf_soln =app.run()
     return image_ref_patch, image_blur_patch, psf_init, psf_soln
 
+def interpolate_sinc(psf, size):
+    shape = (size,) * psf.ndim
+    factor = np.prod(shape) / np.prod(psf.shape)
+    return sp.ifft(sp.resize(sp.fft(psf), shape)) * np.sqrt(factor)
+
+def get_FWHM(x):
+    peak_idx = np.argmax(x)
+    half_max = x[peak_idx] / 2
+    print('FWHM')
+    print(x[peak_idx::1])
+    print(x[peak_idx::-1])
+    return np.argmin(x[peak_idx::1] > half_max) + np.argmin(x[peak_idx::-1] > half_max)
+
+# TODO interpolate along an arbitrary direction of the PSF to measure FWHM in that direction
