e355e1525194555d775e68a7624931bf3f608c4c
Alexander R Toews <artoews@stanford.edu>
2023-09-28 15:05:26 -0700

    modularize PSF estimation

--- psf.py
+++ psf.py
@@ -15,17 +15,21 @@ def shape_without_margin(shape, margin):
 def estimate_psf(image_ref, image_blur, patch_size, psf_size, psf_init=None, start=(50, 70, 31)):
     ndim = image_ref.ndim
     patch = tuple(slice(start[i], start[i] + patch_size + psf_size) for i in range(ndim))
-    if psf_init is None:
-        psf_init = np.zeros((psf_size,) * ndim, dtype=np.complex128)
-    # init[patch_size//2, patch_size//2, patch_size//2] = 1
     image_ref_patch = image_ref[patch]
     image_blur_patch = image_blur[patch]
-    op = model(image_ref_patch, psf_size)
-    image_ref_patch = sp.util.resize(image_ref_patch, shape_without_margin(image_ref_patch.shape, psf_size))
-    image_blur_patch = sp.util.resize(image_blur_patch, shape_without_margin(image_blur_patch.shape, psf_size))
-    app = sp.app.LinearLeastSquares(op, image_blur_patch, x=psf_init.copy(), tol=1e-2, max_iter=1e2)
-    psf_soln =app.run()
+    psf_soln = estimate_psf_patch(image_ref_patch, image_blur_patch, psf_size, psf_init=psf_init)
     return image_ref_patch, image_blur_patch, psf_init, psf_soln
+
+def estimate_psf_patch(image_ref, image_blurred, psf_size, psf_init=None, tol=1e-2, max_iter=100):
+    ndim =image_ref.ndim
+    if psf_init is None:
+        psf_init = np.zeros((psf_size,) * ndim, dtype=np.complex128)
+    op = model(image_ref, psf_size)
+    patch_shape = shape_without_margin(image_ref.shape, psf_size)
+    image_ref = sp.util.resize(image_ref, patch_shape)
+    image_blurred = sp.util.resize(image_blurred, patch_shape)
+    app = sp.app.LinearLeastSquares(op, image_blurred, x=psf_init.copy(), tol=tol, max_iter=max_iter)
+    return app.run()
 
 def interpolate_sinc(psf, size):
     shape = (size,) * psf.ndim
