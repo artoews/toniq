059985e1007f0276f86d3b91814cc64a1610650a
Alexander R Toews <artoews@stanford.edu>
2023-10-03 20:23:56 -0700

    add plotting, saves, and simplify blocks

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from time import time
 
 import analysis
 import dicom
+from plot import plotVolumes
 import psf
 import register
 
@@ -61,9 +62,14 @@ if __name__ == '__main__':
     target_image.data = analysis.equalize(target_image.data, clean_image.data)
     if repeat_image is not None:
         repeat_image.data = analysis.equalize(repeat_image.data, clean_image.data)
+
+    print('Clean data shape', clean_image.data.shape)
+    print('Target data shape', target_image.data.shape)
     
-    # TODO check the ImageVolumes for isotropic resolution
-    voxel_size_mm = target_image.meta.resolution_mm[0]
+    if target_image.is_isotropic:
+        voxel_size_mm = target_image.meta.resolution_mm[0]
+    else:
+        raise ValueError('Isotropic resolution is required, but got: ', target_image.meta.resolution_mm)
 
     if args.snr or args.resolution or args.geometric or args.intensity:
         map_all = False
@@ -72,8 +78,15 @@ if __name__ == '__main__':
     
     mask_empty = analysis.get_mask_empty(clean_image.data)
     mask_signal = analysis.get_mask_signal(clean_image.data, target_image.data)
+
+    if args.plot:
+        volumes = (clean_image.data, target_image.data)
+        titles = ('clean image', 'target image')
+        fig0, tracker0 = plotVolumes(volumes, titles=titles, figsize=(16, 8))
+
     
     if map_all or args.snr:
+
         if repeat_image is None:
             if args.verbose:
                 print('Skipping SNR map, no repeat image provided')
@@ -81,67 +94,105 @@ if __name__ == '__main__':
             if args.verbose:
                 print('Mapping SNR...')
                 start_time = time()
+
             snr, signal, noise_std = analysis.signal_to_noise(clean_image.data, target_image.data, mask_signal, mask_empty)
+
             if args.verbose:
                 print('Done. {:.1f} seconds elapsed.'.format(time() - start_time))
+
+            np.save(path.join(map_dir, 'snr.npy'), snr)
+            np.save(path.join(map_dir, 'signal.npy'), signal)
+            np.save(path.join(map_dir, 'noise.npy'), noise_std)
     
     if map_all or args.resolution:
+
         if args.verbose:
             print('Mapping resolution...')
             start_time = time()
+
         cell_size_pixels = args.cell_size_mm / voxel_size_mm
         patch_size = int(cell_size_pixels * 2)
-        stride = int(cell_size_pixels / 2)
-        psf_soln = psf.estimate_psf_all_in_parallel(clean_image.data, target_image.data, patch_size, stride, psf_size=7)
+        # stride = int(cell_size_pixels / 2)
+        stride = int(cell_size_pixels)
+        # TODO figure out why results are off by 3/8 of a pixel; try plotting PSF and eyeballing what you think the PSF half width should be
+        # clean_input = analysis.denoise(clean_image.data)
+        # target_input = analysis.denoise(target_image.data)
+        clean_input = clean_image.data
+        target_input = target_image.data
+        psf_soln = psf.estimate_psf_all_in_parallel(clean_input, target_input, patch_size, stride, psf_size=7)
+        print('PSF shape', psf_soln.shape)
         fwhm = psf.get_FWHM_in_parallel(psf_soln)
-        if args.verbose:
-            print('Done. {:.1f} seconds elapsed.'.format(time() - start_time))
-    
-    if map_all or args.intensity:  # TODO cleanup!
-        if args.verbose:
-            print('Mapping intensity distortion...')
-            start_time = time()
-        mask_implant, mask_empty, mask_hyper, mask_hypo, mask_artifact = analysis.get_all_masks(clean_image.data, target_image.data)
-        mask_artifact = morphology.dilation(mask_artifact, morphology.ball(2))
-        mask_none = np.zeros_like(mask_implant)
-        mask_to_artifact = analysis.combine_masks(mask_implant, mask_empty, mask_none, mask_none, mask_artifact)
-        mask_to_hypo = analysis.combine_masks(mask_implant, mask_empty, mask_none, mask_hypo, mask_artifact)
-        mask_to_all = analysis.combine_masks(mask_implant, mask_empty, mask_hyper, mask_hypo, mask_artifact)
+        resolution = fwhm * voxel_size_mm  # TODO what if clean and target resolution are different?
+
         if args.verbose:
             print('Done. {:.1f} seconds elapsed.'.format(time() - start_time))
 
-    if map_all or args.geometric:  # TODO cleanup into a function or two!
+        np.save(path.join(map_dir, 'resolution.npy'), resolution)
+
+        if args.plot:
+            psf_slc = np.abs(psf_soln[8, 8, 2, ...])
+            psf_slc = psf_slc / np.max(psf_slc)
+            volumes = (psf_slc, psf_slc)
+            titles = ('PSF with FWHM {} pixels'.format(fwhm[8, 8, 2, :]), 'Same')
+            fig1, tracker1 = plotVolumes(volumes, titles=titles, figsize=(16, 8))
+            vmax = 4
+            volumes = (fwhm[..., 0], fwhm[..., 1], fwhm[..., 2])
+            titles = ('FWHM x [mm]', 'FWHM y [mm]', 'FWHM z [mm]')
+            fig2, tracker2 = plotVolumes(volumes, titles=titles, figsize=(16, 8), vmin=0, vmax=vmax, cmap='twilight', cbar=True)
+
+    if map_all or args.intensity or args.geometric:
+
+        if args.verbose:
+            print('Mapping intensity distortion...')
+            start_time = time()
+
+        mask_implant, mask_empty, mask_hyper, mask_hypo, mask_artifact = analysis.get_all_masks(clean_image.data, target_image.data)
+        # mask_artifact = morphology.dilation(mask_artifact, morphology.ball(2))
+
+        if args.verbose:
+            print('Done. {:.1f} seconds elapsed.'.format(time() - start_time))
+
+        np.save(path.join(map_dir, 'mask_implant.npy'), mask_implant)
+        np.save(path.join(map_dir, 'mask_empty.npy'), mask_empty)
+        np.save(path.join(map_dir, 'mask_hyper.npy'), mask_hyper)
+        np.save(path.join(map_dir, 'mask_hypo.npy'), mask_hypo)
+        np.save(path.join(map_dir, 'mask_artifact.npy'), mask_artifact)
+
+    if map_all or args.geometric:
+
         if args.verbose:
             print('Mapping geometric distortion...')
             start_time = time()
+
         fixed_image = clean_image.data
         moving_image = target_image.data
-        mega_mask = analysis.get_all_masks(fixed_image, moving_image, combine=True) # TODO re-use masks from above if available!
-        fixed_mask = np.logical_not(analysis.get_mask_empty(fixed_image))
+        mega_mask = analysis.combine_masks(mask_implant, mask_empty, mask_hyper, mask_hypo, mask_artifact)
+        
+        fixed_mask = np.logical_not(mask_empty)
         fixed_mask = morphology.erosion(fixed_mask, morphology.ball(2))
         moving_mask = (mega_mask == 2/5)
         moving_mask = morphology.erosion(moving_mask, morphology.ball(2))
-        signal_ref = analysis.get_typical_level(fixed_image)
+
         slc = (slice(25, 175), slice(50, 200), slice(10, 70))
         fixed_image = fixed_image[slc]
         fixed_mask = fixed_mask[slc]
         moving_image = moving_image[slc]
         moving_mask = moving_mask[slc]
+
         active_count = np.sum(moving_mask)
         total_count = np.prod(moving_mask.shape)
-        print('{} of {} pixels active ({:.0f}%)'.format(active_count, total_count, active_count / total_count * 100))
-        fixed_image_masked = fixed_image.copy()
-        fixed_image_masked[~fixed_mask] = 0
-        moving_image_masked = moving_image.copy()
-        moving_image_masked[~moving_mask] = 0
-        result, transform = register.nonrigid(fixed_image, moving_image, fixed_mask, moving_mask)
-        result_masked = register.transform(moving_image_masked, transform)
-        deformation_field = register.get_deformation_field(moving_image, transform)
-        _, jacobian_det = register.get_jacobian(moving_image, transform)
-        np.save(path.join(map_dir, 'deformation_field.npy'), deformation_field)
+
+        if args.verbose:
+            print('{} of {} pixels active ({:.0f}%)'.format(active_count, total_count, active_count / total_count * 100))
+
+        deformation_field, jacobian_det, result, result_masked = analysis.estimate_geometric_distortion(fixed_image, moving_image, fixed_mask, moving_mask)
+
         if args.verbose:
             print('Done. {:.1f} seconds elapsed.'.format(time() - start_time))
 
-    print('Target image', target_image.shape)
-
-    # TODO in each block, save maps and optionally make a plot
+        np.save(path.join(map_dir, 'deformation_field.npy'), deformation_field)
+        np.save(path.join(map_dir, 'jacobian_det.npy'), deformation_field)
+        np.save(path.join(map_dir, 'registration_result.npy'), result)
+        np.save(path.join(map_dir, 'registration_result_masked.npy'), result_masked)
+    
+    plt.show()
