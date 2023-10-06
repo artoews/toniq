23eab58eb244c27682b884db683e75967760f9ca
Alexander R Toews <artoews@stanford.edu>
2023-10-06 12:22:37 -0700

    revise interpolation strategy for FWHM; add comments

--- psf.py
+++ psf.py
@@ -1,13 +1,14 @@
 import numpy as np
+from scipy.ndimage import map_coordinates
 import sigpy as sp
 from multiprocessing import Pool
 
 
 def model(image, psf_size):
-    Z = sp.linop.Resize(image.shape, (psf_size,) * image.ndim)
+    Z = sp.linop.Resize(image.shape, (psf_size,) * image.ndim)  # zero-pad PSF to match image, allowing multiplication in k-space
     F = sp.linop.FFT(image.shape)
     D = sp.linop.Multiply(image.shape, sp.fft(image))
-    C = sp.linop.Resize(shape_without_margin(image.shape, psf_size), image.shape)
+    C = sp.linop.Resize(shape_without_margin(image.shape, psf_size), image.shape) # crop away margin to remove wrapped info from circular convolution
     return C * F.H * D * F * Z
 
 def shape_without_margin(shape, margin):
@@ -21,7 +22,7 @@ def estimate_psf(image_ref, image_blur, patch_size, psf_size, psf_init=None, sta
     psf_soln = estimate_psf_patch(image_ref_patch, image_blur_patch, psf_size, psf_init=psf_init)
     return image_ref_patch, image_blur_patch, psf_init, psf_soln
 
-def estimate_psf_all(image_ref, image_blurred, patch_size, psf_size, stride, tol=1e-2, max_iter=100):
+def estimate_psf_all(image_ref, image_blurred, patch_size, psf_size, stride, tol=1e-4, max_iter=1000):
     nx, ny, nz = image_ref.shape
     zero_init = np.zeros((psf_size,) * 3, dtype=np.complex128)
     nx_pts = np.arange(0, nx - patch_size - psf_size, stride)
@@ -37,7 +38,6 @@ def estimate_psf_all(image_ref, image_blurred, patch_size, psf_size, stride, tol
     return psf
 
 def estimate_psf_all_in_parallel(image_ref, image_blurred, patch_size, stride, psf_size=7, num_workers=8):
-    # TODO in another function (?), compute the FWHM and collect into an array. Interpolate to get a map at the image resolution.
     nx = image_ref.shape[0]
     nx_pts = np.arange(0, nx - patch_size - psf_size, stride)
     pt_splits = np.array_split(nx_pts, num_workers)
@@ -67,7 +67,7 @@ def get_FWHM_in_parallel(psf, num_workers=8):
     result = np.concatenate(result, axis=0)
     return result
 
-def estimate_psf_patch(image_ref, image_blurred, psf_size, psf_init=None, tol=1e-2, max_iter=100, verbose=False):
+def estimate_psf_patch(image_ref, image_blurred, psf_size, psf_init=None, tol=1e-4, max_iter=1000, verbose=True):
     ndim =image_ref.ndim
     if psf_init is None:
         psf_init = np.zeros((psf_size,) * ndim, dtype=np.complex128)
@@ -78,10 +78,10 @@ def estimate_psf_patch(image_ref, image_blurred, psf_size, psf_init=None, tol=1e
     app = sp.app.LinearLeastSquares(op, image_blurred, x=psf_init.copy(), tol=tol, max_iter=max_iter, show_pbar=verbose)
     return app.run()
 
-def interpolate_sinc(psf, size):
-    shape = (size,) * psf.ndim
-    factor = np.prod(shape) / np.prod(psf.shape)
-    return sp.ifft(sp.resize(sp.fft(psf), shape)) * np.sqrt(factor)
+def interpolate(psf, factor):
+    nx, ny, nz = psf.shape
+    coords = np.mgrid[:nx:1/factor, :ny:1/factor, :nz:1/factor]
+    return map_coordinates(psf, coords, order=2)
 
 def get_FWHM_from_many_psf(psf):
     nx, ny, nz = psf.shape[:3]
@@ -92,28 +92,26 @@ def get_FWHM_from_many_psf(psf):
                 fwhm[ix, iy, iz, :] = get_FWHM_from_psf(psf[ix, iy, iz, ...])
     return fwhm
 
-def get_FWHM_from_psf(psf, interp_factor=8):
+def get_FWHM_from_psf(psf):
     psf = np.abs(psf)
-    psf_size = psf.shape[0]
-    psf_int = interpolate_sinc(psf, psf_size * interp_factor)  # TODO try sigpy.interpolate
-    max_idx = np.unravel_index(np.argmax(psf_int), psf_int.shape)
-    max_val = psf_int[max_idx]
-    fwhm_x = fwhm(psf_int[:, max_idx[1], max_idx[2]], max_val) / interp_factor
-    fwhm_y = fwhm(psf_int[max_idx[0], :, max_idx[2]], max_val) / interp_factor
-    fwhm_z = fwhm(psf_int[max_idx[0], max_idx[1], :], max_val) / interp_factor
+    max_idx = np.unravel_index(np.argmax(psf), psf.shape)
+    fwhm_x = fwhm(psf[:, max_idx[1], max_idx[2]], im=max_idx[0])
+    fwhm_y = fwhm(psf[max_idx[0], :, max_idx[2]], im=max_idx[1])
+    fwhm_z = fwhm(psf[max_idx[0], max_idx[1], :], im=max_idx[2])
     return fwhm_x, fwhm_y, fwhm_z
 
-def fwhm(x, max_val=None, max_idx=None):
-    if max_idx is None or max_val is None:
-        max_idx = np.argmax(x)
-        max_val = x[max_idx]
-    half_max = max_val / 2
-    return np.argmin(x[max_idx::1] > half_max) + np.argmin(x[max_idx::-1] > half_max) - 1
+def fwhm(x, im=None):
+    if im is None:
+        im = np.argmax(x)
+    hm = x[im] / 2
+    if im < 1 or im >= len(x) - 1:
+        return 0
+    i_hm = im - np.argmin(x[im::-1] > hm)  # just left of half-max
+    i_hm_1 = find_root(i_hm, x[i_hm] - hm, i_hm + 1, x[i_hm + 1] - hm)
+    i_hm = im + np.argmin(x[im::1] > hm)  # just right of half-max
+    i_hm_2 = find_root(i_hm - 1, x[i_hm - 1] - hm, i_hm, x[i_hm] - hm)
+    return i_hm_2 - i_hm_1
 
-def get_FWHM(x):
-    peak_idx = np.argmax(x)
-    half_max = x[peak_idx] / 2
-    # print('FWHM')
-    # print(x[peak_idx::1])
-    # print(x[peak_idx::-1])
-    return np.argmin(x[peak_idx::1] > half_max) + np.argmin(x[peak_idx::-1] > half_max)
+def find_root(x1, y1, x2, y2):
+    # find zero-crossing of line through points (x1, y1) and (x2, y2)
+    return x1 - y1 * (x2 - x1) / (y2 - y1)
