ecae524f671a7cd604bef9e07fd3d7b93b873f7d
Alexander R Toews <artoews@stanford.edu>
2023-10-17 16:04:20 -0700

    implement kspace division method; hack psf shape to work for this

--- psf.py
+++ psf.py
@@ -4,6 +4,7 @@ from scipy.linalg import circulant, lstsq
 from scipy.signal import convolve2d
 import sigpy as sp
 from multiprocessing import Pool
+from util import safe_divide
 
 def batch_with_overlap(volume, overlap, num_batches, axis=-1):
     starts = np.arange(0, volume.shape[axis] - overlap)
@@ -14,6 +15,8 @@ def batch_with_overlap(volume, overlap, num_batches, axis=-1):
 def map_psf(image_in, image_out, mask, patch_shape, psf_shape, stride, mode, num_workers=1):
     # batched dimension is not strided
     batch_axis = 2
+    if mode == 'kspace':
+        psf_shape = (1, 1, 1)
 
     if image_in.shape != image_out.shape or mask.shape != image_out.shape:
         raise ValueError('Image input, output and mask must have same shape; got {}, {}, {}'.format(image_in.shape, image_out.shape, mask.shape))
@@ -41,7 +44,11 @@ def map_psf(image_in, image_out, mask, patch_shape, psf_shape, stride, mode, num
     strides = np.roll((1, stride, stride), batch_axis)
     patch_locs = tuple(np.arange(0, image_in.shape[i] - patch_shape[i] - psf_shape[i] + 1, strides[i]) for i in range(3))
     num_locs = tuple(len(p) for p in patch_locs)
+    if mode == 'kspace':
+        psf_shape = patch_shape
     psf = np.zeros(num_locs + psf_shape, dtype=np.complex128)
+    if mode == 'kspace':
+        psf_shape = (1, 1, 1)
     for patch_loc in itertools.product(*patch_locs):
         slc = tuple(slice(patch_loc[i], patch_loc[i] + patch_shape[i] + psf_shape[i] - 1) for i in range(3))
         patch_in = image_in[slc]
@@ -49,16 +56,26 @@ def map_psf(image_in, image_out, mask, patch_shape, psf_shape, stride, mode, num
         if mask is not None and not np.all(mask[slc]):
             soln = np.zeros(psf_shape, dtype=np.complex128)
         elif mode == 'iterative':
-            tol = 1e-6
+            tol = 1e-4  # was 1e-6
             max_iter = 1e3
             soln = estimate_psf_iterative(patch_in, patch_out, psf_shape, psf_init, tol, max_iter)
         elif mode == 'direct':
             soln = estimate_psf_direct(patch_in, patch_out, psf_shape)
+        elif mode == 'kspace':
+            soln = estimate_psf_kspace(patch_in, patch_out)
         idx = tuple(patch_loc[i] // strides[i] for i in range(3))
         psf[idx] = soln
     return psf
 
-def estimate_psf_iterative(image_in, image_out, psf_shape, psf_init, tol, max_iter, verbose=False):
+def estimate_psf_kspace(patch_in, patch_out):
+    # TODO set thresh based on noise std?
+    kspace_in = sp.fft(patch_in)
+    kspace_out = sp.fft(patch_out)
+    kspace_quotient = safe_divide(kspace_out, kspace_in, thresh=1e-2)
+    psf = np.abs(sp.ifft(kspace_quotient))
+    return psf
+
+def estimate_psf_iterative(image_in, image_out, psf_shape, psf_init, tol, max_iter, verbose=True):
     # image_in and image_out having a margin to accomodate circular convolution
     A = model_op(image_in, psf_shape)
     # A = model_matrix_op(image_in, psf_shape)  # slower
