ef92860652ce9b2dfc9fab9453c81397de36aa3f
Alexander R Toews <artoews@stanford.edu>
2023-10-26 12:07:32 -0700

    decouple itk init from non-rigid registration

--- register.py
+++ register.py
@@ -36,22 +36,30 @@ def transform(image, elastix_parameters):
         image_transformed = image_transformed.astype(np.float)
     return image_transformed
 
-def nonrigid(fixed_image, moving_image, fixed_mask, moving_mask, verbose=True):
-    parameter_object = itk.ParameterObject.New()  # slow
+def setup_nonrigid(verbose=True):
+    if verbose:
+        print('Beginning ITK setup...')
+    t0 = time()
+    parameter_object = itk.ParameterObject.New()  # this is the slow line
     default_bspline_parameter_map = parameter_object.GetDefaultParameterMap('bspline', 1)
-    # default_bspline_parameter_map['FinalGridSpacingInPhysicalUnits'] = ['30']
-    default_bspline_parameter_map['FinalGridSpacingInPhysicalUnits'] = ['20']
-    # default_bspline_parameter_map['Metric'] = ['AdvancedMeanSquares']
-    # default_bspline_parameter_map['Metric'] = ['AdvancedNormalizedCorrelation']
+    # 20-30 seems to work well empirically; but below 30, start to see some displacement showing up in y & z maps when they should be zero
+    default_bspline_parameter_map['FinalGridSpacingInPhysicalUnits'] = ['30']  
     default_bspline_parameter_map['Metric'] = ['AdvancedMattesMutualInformation']
-    # default_bspline_parameter_map['Metric'] = ['NormalizedMutualInformation']
     default_bspline_parameter_map['BSplineTransformSplineOrder'] = ['3']
     default_bspline_parameter_map['ImageSampler'] = ['Full']
     parameter_object.AddParameterMap(default_bspline_parameter_map)
     if verbose:
         print(parameter_object)
-    return elastix_registration(fixed_image, moving_image, fixed_mask, moving_mask, parameter_object, verbose=False)
+        print('Done ITK setup. {:.2f} seconds elapsed'.format(time() - t0))
+    return parameter_object
 
+def nonrigid(fixed_image, moving_image, fixed_mask, moving_mask, verbose=True):
+    parameter_object = setup_nonrigid(verbose=verbose)
+    print(fixed_image.shape, fixed_image.dtype)
+    print(moving_image.shape, moving_image.dtype)
+    print(fixed_mask.shape, fixed_mask.dtype)
+    print(moving_mask.shape, moving_mask.dtype)
+    return elastix_registration(fixed_image, moving_image, fixed_mask, moving_mask, parameter_object, verbose=False)
 
 def rigid(fixed_image, moving_image, fixed_mask, moving_mask, verbose=False):
     parameter_object = itk.ParameterObject.New()  # slow
@@ -65,7 +73,7 @@ def get_deformation_field(moving_image, transform):
     # from https://github.com/InsightSoftwareConsortium/ITKElastix/blob/main/examples/ITK_Example11_Transformix_DeformationField.ipynb
     moving_image = itk.image_from_array(moving_image)
     field = itk.transformix_deformation_field(moving_image, transform)
-    return np.asarray(field).astype(np.float)
+    return np.asarray(field).astype(np.float)[..., ::-1]  # in 2D, these dimesnions seem to be swapped. not sure about 3D
 
 def get_jacobian(moving_image, transform):
     # from https://github.com/InsightSoftwareConsortium/ITKElastix/blob/main/examples/ITK_Example10_Transformix_Jacobian.ipynb
