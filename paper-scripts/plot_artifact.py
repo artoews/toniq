0a1142b95797b75e3ad8afebe84fd43300925ec3
Alexander R Toews <artoews@stanford.edu>
2024-01-14 13:50:53 -0800

    add mask overlay

--- paper-scripts/plot_artifact.py
+++ paper-scripts/plot_artifact.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from os import path
 
-from plot import plotVolumes
+from plot import plotVolumes, overlay_mask
 from util import safe_divide
 
 def plot_progression(reference, target, map_artifact, signal_ref):
@@ -59,3 +59,41 @@ def plot_artifact_results(images, maps_artifact, signal_ref, rbw, save_dir=None)
         axes[i, 5].tick_params(labelsize=fs*0.75)
     if save_dir is not None:
         plt.savefig(path.join(save_dir, 'validation_artifact.png'), dpi=300)
+
+def plot_artifact_results_overlay(images, maps_artifact, signal_ref, rbw, save_dir=None):
+    image_ref = images[0]
+    num_trials = len(maps_artifact)
+    slc = (slice(None), slice(None), image_ref.shape[2] // 2)
+    fs = 20
+    fig, axes = plt.subplots(nrows=num_trials, ncols=6, figsize=(18, 6), gridspec_kw={'width_ratios': [1, 1, 1, 1, 1, 0.1]})
+    if num_trials == 1:
+        axes = axes[None, :]
+    for ax in axes.ravel():
+        ax.set_xticks([])
+        ax.set_yticks([])
+    axes[0, 0].imshow(image_ref[slc], cmap='gray', vmin=0, vmax=1)
+    axes[0, 0].set_title('Plastic', fontsize=fs)
+    axes[0, 1].set_title('Metal', fontsize=fs)
+    axes[0, 3].set_title('Relative Error', fontsize=fs)
+    axes[0, 4].set_title('+ Mean Filter', fontsize=fs)
+    axes[0, 2].set_title('Metal + Filtered Error', fontsize=fs)
+    # axes[0, 3].set_title('Intensity Artifact Map')
+    # axes[0, 4].set_title('Intensity Artifact Mask')
+    for i in range(num_trials):
+        image_i = images[1+i]
+        error_i = image_i - image_ref
+        normalized_error_i = safe_divide(error_i, signal_ref)
+        mask_artifact = np.zeros_like(maps_artifact[i])
+        mask_artifact[maps_artifact[i] > 0.3] = 0.3
+        mask_artifact[maps_artifact[i] < -0.3] = -0.3
+        if i > 0: plt.delaxes(axes[i, 0])
+        im1 = axes[i, 1].imshow(image_i[slc], cmap='gray', vmin=0, vmax=1)
+        im2 = axes[i, 3].imshow(normalized_error_i[slc], cmap='RdBu_r', vmin=-1, vmax=1)
+        axes[i, 4].imshow(maps_artifact[i][slc], cmap='RdBu_r', vmin=-1, vmax=1)
+        axes[i, 2].imshow(image_i[slc], cmap='gray', vmin=0, vmax=1)
+        axes[i, 1].set_ylabel('RBW={:.3g}kHz'.format(rbw[i]), fontsize=fs)
+        plt.colorbar(im2, cax=axes[i, 5], ticks=[-1, 0, 1])
+        axes[i, 5].tick_params(labelsize=fs*0.75)
+        overlay_mask(axes[i, 2], np.abs(mask_artifact[slc]) >= 0.3, color=[0, 0, 255], alpha=50)
+    if save_dir is not None:
+        plt.savefig(path.join(save_dir, 'validation_artifact_overlay.png'), dpi=300) 
