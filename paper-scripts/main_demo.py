1da938e824a378ada50d5ff127c3e705c1f1ad3d
Alexander R Toews <artoews@stanford.edu>
2024-01-16 17:14:39 -0800

    update demo for new phantom data

--- paper-scripts/main_demo.py
+++ paper-scripts/main_demo.py
@@ -3,6 +3,8 @@ import numpy as np
 from os import path
 import scipy.stats as stats
 
+from plot import plotVolumes
+
 def load_outputs(root, subfolder):
     data = np.load(path.join(root, subfolder, 'outputs.npz'))
     for var in data:
@@ -26,23 +28,19 @@ styles = ['dotted', 'solid', 'dashed']
 image_kwargs = {'vmin': 0, 'vmax': 1, 'cmap': 'gray'}
 artifact_kwargs = {'vmin': -1, 'vmax': 1, 'cmap': 'RdBu_r'}
 distortion_kwargs = {'vmin': -2, 'vmax': 2, 'cmap': 'RdBu_r'}
-resolution_kwargs = {'vmin': 0, 'vmax': 2, 'cmap': 'viridis'}
+resolution_kwargs = {'vmin': 1, 'vmax': 3, 'cmap': 'viridis'}
 noise_kwargs = {'vmin': 0, 'vmax': 1, 'cmap': 'viridis'}
 
 ## scan time reference
-# FSE @ 500 kHz = 1:21 (81s)
-# FSE @ 250 kHz = 1:21 (81s)
-# FSE @ 125 kHz = 1:21 (81s)
-# FSE @ 62  KHz = 1:53 (113s)
-# MSL @ 250 kHz = 4:29 (269s)
-# MSL @ 125 kHz = 6:44 (404s)
+# FSE @ [250, 125, 100] kHz = 2:20 (140s)
+# MSL @ [250, 125, 100] kHz = 7:42 (462s)
 
 ## data directories
-fse_dir = '/Users/artoews/root/code/projects/metal-phantom/abstract/demo-fse-250'
-msl_dir = ['/Users/artoews/root/code/projects/metal-phantom/abstract/demo-msl-250',
-           '/Users/artoews/root/code/projects/metal-phantom/abstract/demo-msl-125']
+fse_dir = '/Users/artoews/root/code/projects/metal-phantom/bmr/demo-fse-250'
+msl_dir = ['/Users/artoews/root/code/projects/metal-phantom/bmr/demo-msl-250',
+           '/Users/artoews/root/code/projects/metal-phantom/bmr/demo-msl-100']
 seq_names = ['FSE', 'MAVRIC-SL', 'MAVRIC-SL']
-scan_times = [81, 269, 404]  # seconds
+scan_times = [140, 462, 462]  # seconds
 dirs = [fse_dir] + msl_dir
 
 ## slice to show
@@ -105,8 +103,8 @@ axs[-1, 0].annotate("readout",
 for i in range(len(dirs)):
     load_outputs(dirs[i], 'distortion')
     rbw_i = rbw[1]
-    results_masked_i = results_masked[0]
-    # results_masked_i = results[0]
+    # results_masked_i = results_masked[0]
+    results_masked_i = results[0]
     print('deformation fields', deformation_fields.shape)
     print('results masked', results_masked_i.shape)
     deformation_fields_i = deformation_fields[0]
@@ -116,12 +114,14 @@ for i in range(len(dirs)):
     measured_deformation = measured_deformation * result_mask
     im = axs[i, 3].imshow(measured_deformation[slc], **distortion_kwargs)
     x = np.linspace(0, 2, 50)
-    y = np.abs(measured_deformation[np.abs(measured_deformation)>0].ravel())
+    y = np.abs(measured_deformation.ravel())
     density = stats.gaussian_kde(y)
     axs[-1, 3].plot(x, density(x), linestyle=styles[i])
 axs[-1, 3].set_xticks([0, 1, 2])
 axs[-1, 3].set_xlabel('Abs. Displacement\n(pixels)')
+axs[-1, 3].set_ylim([0, 1])
 cb = fig.colorbar(im, ax=axs[0, 3], ticks=[-2, -1, 0, 1, 2], label='Displacement (pixels)')
+
 
 ## resolution column
 for i in range(len(dirs)):
@@ -129,14 +129,14 @@ for i in range(len(dirs)):
     print('loaded resolution outputs')
     res_x = fwhms[0][..., 0]
     im = axs[i, 4].imshow(res_x[slc], **resolution_kwargs)
-    x = np.linspace(0, 2, 50)
+    x = np.linspace(0, 3, 50)
     y = res_x[res_x>0].ravel()
     density = stats.gaussian_kde(y)
     axs[-1, 4].plot(x, density(x), linestyle=styles[i])
-axs[-1, 4].set_xlim([1, 2])
-axs[-1, 4].set_xticks([1, 1.5, 2])
-axs[-1, 4].set_xlabel('FWHM\n(pixels)')
-cb = fig.colorbar(im, ax=axs[0, 4], ticks=[0, 1, 2], label='FWHM (pixels)')
+axs[-1, 4].set_xlim([1, 3])
+axs[-1, 4].set_xticks([1, 2, 3])
+axs[-1, 4].set_xlabel('FWHM\n(mm)')
+cb = fig.colorbar(im, ax=axs[0, 4], ticks=[1, 2, 3], label='FWHM (mm)')
 
 ## noise column
 for i in range(len(dirs)):
