244217a11bcf6e8954ec67167abe0a684cedd418
Alexander R Toews <artoews@stanford.edu>
2024-01-19 17:48:57 -0800

    update artifact analysis to use RBW-matched pairs

--- paper-scripts/main_artifact.py
+++ paper-scripts/main_artifact.py
@@ -5,7 +5,8 @@ import numpy as np
 from os import path, makedirs
 
 from masks import get_mask_extrema, get_typical_level
-from plot_artifact import plot_artifact_results, plot_artifact_results_overlay, plot_progression
+from plot_artifact import plot_artifact_results, plot_progression
+from plot import plotVolumes
 
 from util import safe_divide, equalize, load_series, save_args
 
@@ -14,7 +15,7 @@ slc = (slice(40, 160), slice(65, 185), slice(10, 50))
 p = argparse.ArgumentParser(description='Intensity artifact analysis of 2DFSE multi-slice image volumes with varying readout bandwidth.')
 p.add_argument('root', type=str, help='path where outputs are saved')
 p.add_argument('-e', '--exam_root', type=str, default=None, help='directory where exam data exists in subdirectories')
-p.add_argument('-s', '--series_list', type=str, nargs='+', default=None, help='list of exam_root subdirectories to be analyzed, with the first serving as plastic reference')
+p.add_argument('-s', '--series_list', type=str, nargs='+', default=None, help='list of exam_root subdirectories to be analyzed in RBW-matched (plastic, metal) pairs')
 
 if __name__ == '__main__':
 
@@ -29,22 +30,22 @@ if __name__ == '__main__':
 
         images = [load_series(args.exam_root, series_name) for series_name in args.series_list]
         
-        rbw = np.array([image.meta.readoutBandwidth_kHz for image in images[1:]])
+        rbw = np.array([image.meta.readoutBandwidth_kHz for image in images[::2]])
 
         images = np.stack([image.data for image in images])
 
         images = equalize(images)
         
-        signal_ref = get_typical_level(images[0])
+        signal_refs = np.stack([get_typical_level(image) for image in images[::2]])
 
         if slc is not None:
             images = images[(slice(None),) + slc]
-            signal_ref = signal_ref[slc]
+            signal_refs = signal_refs[(slice(None),) + slc]
 
         maps_artifact = []
-        for i in range(1, len(images)):
-            print('trial', i)
-            normalized_error = safe_divide(images[i] - images[0], signal_ref)
+        for i in range(len(images) // 2):
+            print('trial {} of {}'.format(i, len(images)//2))
+            normalized_error = safe_divide(images[2*i+1] - images[2*i], signal_refs[i])
             _, map_artifact = get_mask_extrema(normalized_error, 0.3, 'mean', abs_margin=False)
             maps_artifact.append(map_artifact)
         maps_artifact = np.stack(maps_artifact)
@@ -52,7 +53,7 @@ if __name__ == '__main__':
         np.savez(path.join(save_dir, 'outputs.npz'),
             images=images,
             maps_artifact=maps_artifact,
-            signal_ref=signal_ref,
+            signal_refs=signal_refs,
             rbw=rbw
          )
     
@@ -67,9 +68,11 @@ if __name__ == '__main__':
     figs = [None] * num_trials
     trackers = [None] * num_trials
     for i in range(num_trials):
-        figs[i], trackers[i] = plot_progression(images[0], images[1+i], maps_artifact[i], signal_ref)
+        # figs[i], trackers[i] = plot_progression(images[2*i], images[2*i+1], maps_artifact[i], signal_refs[i])
+        pass
 
-    plot_artifact_results(images, maps_artifact, signal_ref, rbw, save_dir=save_dir)
-    # plot_artifact_results_overlay(images, maps_artifact, signal_ref, rbw, save_dir=save_dir)
+    plot_artifact_results(images, maps_artifact, signal_refs, rbw, save_dir=save_dir)
+
+    # fig, tracker = plotVolumes((images[0], signal_refs[0]), vmin=0.3, vmax=0.8)
 
     plt.show()
--- paper-scripts/plot_artifact.py
+++ paper-scripts/plot_artifact.py
@@ -23,75 +23,31 @@ def plot_progression(reference, target, map_artifact, signal_ref):
     fig, tracker = plotVolumes(volumes, 1, len(volumes), titles=titles, figsize=(16, 8), vmin=-1, vmax=1)
     return fig, tracker
 
-def plot_artifact_results(images, maps_artifact, signal_ref, rbw, save_dir=None):
-    image_ref = images[0]
+def plot_artifact_results(images, maps_artifact, signal_refs, rbw, save_dir=None):
+    shape = images[0].shape
     num_trials = len(maps_artifact)
-    slc_xy = (slice(None), slice(None), image_ref.shape[2] // 2)
-    slc_xz = (slice(None), image_ref.shape[1] // 2, slice(None))
+    slc_xy = (slice(None), slice(None), shape[2] // 2)
+    slc_xz = (slice(None), shape[1] // 2, slice(None))
     fs = 20
-    fig, axes = plt.subplots(nrows=num_trials, ncols=5, figsize=(18, 6), gridspec_kw={'width_ratios': [1, 1, 1, 1, 0.1]})
+    fig = plt.figure(figsize=(18, 6), layout='constrained')
+    axes = fig.subplots(nrows=num_trials, ncols=4)
 
     if num_trials == 1:
         axes = axes[None, :]
-    imshow2(axes[0, 0], image_ref, slc_xy, slc_xz)
-    # axes[0, 0].imshow(image_ref[slc_xy], cmap='gray', vmin=0, vmax=1)
     axes[0, 0].set_title('Plastic', fontsize=fs)
     axes[0, 1].set_title('Metal', fontsize=fs)
-    axes[0, 2].set_title('Relative Error', fontsize=fs)
-    axes[0, 3].set_title('Mean Filter', fontsize=fs)
+    axes[0, 2].set_title('Error', fontsize=fs)
+    axes[0, 3].set_title('Filtered Error', fontsize=fs)
     for i in range(num_trials):
-        image_i = images[1+i]
-        error_i = image_i - image_ref
-        normalized_error_i = safe_divide(error_i, signal_ref)
-        mask_artifact = np.zeros_like(maps_artifact[i])
-        mask_artifact[maps_artifact[i] > 0.3] = 0.3
-        mask_artifact[maps_artifact[i] < -0.3] = -0.3
-        if i > 0: plt.delaxes(axes[i, 0])
-        # axes[i, 1].imshow(image_i[slc_xy], cmap='gray', vmin=0, vmax=1)
-        # im2 = axes[i, 2].imshow(normalized_error_i[slc_xy], cmap='RdBu_r', vmin=-1, vmax=1)
-        # axes[i, 3].imshow(maps_artifact[i][slc_xy], cmap='RdBu_r', vmin=-1, vmax=1)
-        axes[i, 1].set_ylabel('RBW={:.3g}kHz'.format(rbw[i]), fontsize=fs)
-        imshow2(axes[i, 1], image_i, slc_xy, slc_xz)
-        im, _ = imshow2(axes[i, 2], normalized_error_i, slc_xy, slc_xz, cmap='RdBu_r', vmin=-1, vmax=1)
+        axes[i, 0].set_ylabel('RBW={:.3g}kHz'.format(rbw[i]), fontsize=fs)
+        error = safe_divide(images[2*i+1] - images[2*i], signal_refs[i])
+        imshow2(axes[i, 0], images[2*i], slc_xy, slc_xz)
+        imshow2(axes[i, 1], images[2*i+1], slc_xy, slc_xz)
+        im, _ = imshow2(axes[i, 2], error, slc_xy, slc_xz, cmap='RdBu_r', vmin=-1, vmax=1)
         imshow2(axes[i, 3], maps_artifact[i], slc_xy, slc_xz, cmap='RdBu_r', vmin=-1, vmax=1)
-        plt.colorbar(im, cax=axes[i, 4], ticks=[-1, 0, 1])
-        axes[i, 4].tick_params(labelsize=fs*0.75)
+    cbar = fig.colorbar(im, ax=axes[:, 3], ticks=[-1, -0.5, 0, 0.5, 1])
+    # cbar = plt.colorbar(im, cax=axes[:, 4], ticks=[-1, -0.5, 0, 0.5, 1])
+    cbar.ax.set_yticklabels(['-100%', '-50%', '0', '50%', '+100%'])
+    # axes[:, 4].tick_params(labelsize=fs*0.75)
     if save_dir is not None:
         plt.savefig(path.join(save_dir, 'validation_artifact.png'), dpi=300)
-
-def plot_artifact_results_overlay(images, maps_artifact, signal_ref, rbw, save_dir=None):
-    image_ref = images[0]
-    num_trials = len(maps_artifact)
-    slc = (slice(None), slice(None), image_ref.shape[2] // 2)
-    fs = 20
-    fig, axes = plt.subplots(nrows=num_trials, ncols=6, figsize=(18, 6), gridspec_kw={'width_ratios': [1, 1, 1, 1, 1, 0.1]})
-    if num_trials == 1:
-        axes = axes[None, :]
-    for ax in axes.ravel():
-        ax.set_xticks([])
-        ax.set_yticks([])
-    axes[0, 0].imshow(image_ref[slc], cmap='gray', vmin=0, vmax=1)
-    axes[0, 0].set_title('Plastic', fontsize=fs)
-    axes[0, 1].set_title('Metal', fontsize=fs)
-    axes[0, 3].set_title('Relative Error', fontsize=fs)
-    axes[0, 4].set_title('+ Mean Filter', fontsize=fs)
-    axes[0, 2].set_title('Metal + Filtered Error', fontsize=fs)
-    # axes[0, 3].set_title('Intensity Artifact Map')
-    for i in range(num_trials):
-        image_i = images[1+i]
-        error_i = image_i - image_ref
-        normalized_error_i = safe_divide(error_i, signal_ref)
-        mask_artifact = np.zeros_like(maps_artifact[i])
-        mask_artifact[maps_artifact[i] > 0.3] = 0.3
-        mask_artifact[maps_artifact[i] < -0.3] = -0.3
-        if i > 0: plt.delaxes(axes[i, 0])
-        im1 = axes[i, 1].imshow(image_i[slc], cmap='gray', vmin=0, vmax=1)
-        im2 = axes[i, 3].imshow(normalized_error_i[slc], cmap='RdBu_r', vmin=-1, vmax=1)
-        axes[i, 4].imshow(maps_artifact[i][slc], cmap='RdBu_r', vmin=-1, vmax=1)
-        axes[i, 2].imshow(image_i[slc], cmap='gray', vmin=0, vmax=1)
-        axes[i, 1].set_ylabel('RBW={:.3g}kHz'.format(rbw[i]), fontsize=fs)
-        plt.colorbar(im2, cax=axes[i, 5], ticks=[-1, 0, 1])
-        axes[i, 5].tick_params(labelsize=fs*0.75)
-        overlay_mask(axes[i, 2], np.abs(mask_artifact[slc]) >= 0.3, color=[0, 0, 255], alpha=50)
-    if save_dir is not None:
-        plt.savefig(path.join(save_dir, 'validation_artifact_overlay.png'), dpi=300) 
