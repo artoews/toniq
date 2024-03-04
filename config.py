d86a9bc277c904dfea3bced8523dbb3ebe8a3e3a
Alexander R Toews <artoews@stanford.edu>
2024-03-03 21:54:13 -0800

    add read_config

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
+import yaml
+
+def read_config(file):
+    with open(file, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
+
 def parse_slice(config):
     return tuple(slice(start, stop) for start, stop in config['params']['slice'])
