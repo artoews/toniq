5e528f245cd01bc2ca1a9e36a294c3afe82e572f
Alexander R Toews <artoews@stanford.edu>
2024-03-20 11:46:55 -0700

    remove obsolete methods

--- intensity.py
+++ intensity.py
@@ -14,17 +14,3 @@ def map_snr(image1, image2, mask, filter_size=10, min_coverage=0.25):
     snr = np.divide(signal, noise, out=np.zeros_like(signal), where=noise > 0)
     snr[~mask] = 0
     return snr, signal, noise
-
-def noise_std(image1, image2, filter_size=10):
-    footprint = morphology.cube(filter_size)
-    image_diff = np.abs(image2) - np.abs(image1)
-    noise = ndi.generic_filter(image_diff, np.std, footprint=footprint) / np.sqrt(2)
-    return noise
-
-def predict_fse_intensity_error(field, profile, pixel_bw=None):
-    image = profile(field) # assume uniform image with unit-strength signal
-    if pixel_bw is not None:
-        pass
-        # TODO I have asked how to apply deformation field to transform image here: https://github.com/SuperElastix/elastix/discussions/1016
-    error = image - 1
-    return error
