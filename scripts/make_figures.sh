ab9ae3230efb229c9453c2b2377dde41bf491157
Alexander R Toews <artoews@stanford.edu>
2024-04-04 11:24:30 -0700

    update to apr4 results

--- scripts/make_figures.sh
+++ scripts/make_figures.sh
@@ -4,11 +4,11 @@ if [ "$1" == "-h" ]; then
 fi
 
 python scripts/figure1.py $1 -c1 config/mar4-fse125.yml -c2 config/mar4-msl125.yml
-python scripts/figure2.py $1 --out out/mar20/mar4-fse125
+python scripts/figure2.py $1 --out out/apr4/mar4-fse125
 python scripts/figure4.py $1 -c config/mar4-fse125.yml
 # python scripts/figure5.py $1 # slow
-python scripts/figure6.py $1 --out1 out/mar20/mar4-fse125 --out2 out/mar20/mar4-msl125
-python scripts/figure7.py $1 --out out/mar20/mar4-fse125
+python scripts/figure6.py $1 --out1 out/apr4/mar4-fse125 --out2 out/apr4/mar4-msl125
+python scripts/figure7.py $1 --out out/apr4/mar4-fse125
 python scripts/figure8.py $1 
-python scripts/figure9.py $1 -c config/mar4-fse125.yml -l # very slow without load argument (-l)
-python scripts/figure10.py $1 --out1 out/mar20/mar4-msl125 --out2 out/mar20/mar4-msl63
+# python scripts/figure9.py $1 -c config/mar4-fse125.yml -l # very slow without load argument (-l)
+python scripts/figure10.py $1 --out1 out/apr4/mar4-msl125 --out2 out/apr4/mar4-msl63
