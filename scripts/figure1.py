b6eb993e14cb47df257d5124cefdb2787d701021
Alexander R Toews <artoews@stanford.edu>
2024-04-04 10:14:22 -0700

    add slice labels

--- scripts/figure1.py
+++ scripts/figure1.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
         for j in range(len(slices)):
             plot_panel(axes[i, j], images[i][slices[j]])
             if j < len(slices) - 1:
-                label_slice_pos(axes[i, -1], -1, slices[j], slc)
+                label_slice_pos(axes[i, -1], -1, slices[j], slc, label=j+1)
                 label_slice_pos(axes[i, j], 1, slices[-1], slc)
             axes[0, j].set_title(slice_names[j])
             # label_panel(fig, axes[i, j], labels[i][j])
