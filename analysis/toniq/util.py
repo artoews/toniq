4606f7a0824c997dc6711b7d299607f2d2745c01
Alexander R Toews <artoews@stanford.edu>
2024-05-26 16:04:57 -0700

    add docs

--- analysis/toniq/util.py
+++ analysis/toniq/util.py
@@ -1,28 +1,36 @@
+"""Miscellaneous utility functions.
+
+"""
+import argparse
 import inspect
 import json
+import numpy.typing as npt
 import numpy as np
 import sigpy as sp
 import sys
 
 from os import path
-from pathlib import Path
 from skimage import filters, morphology
 from time import time
 
-from toniq import dicom
+def equalize(
+        images: npt.NDArray | list[npt.NDArray],
+        pct: float = 99,
+        axis: int = 0
+        ) -> npt.NDArray | list[npt.NDArray]:
+    """ Rescale the intensity levels for a stack of images so that the foreground intensities are synchronized.
 
-def load_series(exam_root, series_name):
-    series_path = path.join(exam_root, series_name)
-    files = Path(series_path).glob('*MRDC*')
-    image = dicom.load_series(files)
-    return image
+    The median intensity in the foreground of each image is synchronized with the first image in the stack. 
+    Image foreground is determined by taking all pixels above the Otsu threshold.
 
-def load_series_from_path(series_path):
-    files = Path(series_path).glob('*MRDC*')
-    image = dicom.load_series(files)
-    return image
+    Args:
+        images (npt.NDArray | list[npt.NDArray]): stack of images
+        pct (float, optional): percentile used to normalize the first image in the stack. Defaults to 99.
+        axis (int, optional): axis along which images are stacked, if not already in list format. Defaults to 0.
 
-def equalize(images, pct=99, axis=0):
+    Returns:
+        npt.NDArray | list[npt.NDArray]: equalized image stack
+    """
     if type(images) == np.ndarray:
         images = list(np.moveaxis(images, axis, 0))
         is_array = True
@@ -40,16 +48,33 @@ def equalize(images, pct=99, axis=0):
         images = np.moveaxis(np.stack(images), 0, axis)
     return images
 
-def normalize(image, pct=99):
+def normalize(
+        image: npt.NDArray,
+        pct: float = 99
+        ) -> npt.NDArray:
+    """ Normalize image pixels by the value of a given percentile. """
     return image / np.percentile(np.abs(image), pct)
 
-def resize_image_matrix(image, shape):
+def resize_image_matrix(
+        image: npt.NDArray,
+        shape: tuple[int]
+        ) -> npt.NDArray:
+    """ Resize image by sinc interpolation. """
     return np.abs(sp.ifft(sp.resize(sp.fft(image), shape)))
 
-def safe_divide(divisor, dividend, thresh=0):
+def safe_divide(
+        divisor: npt.NDArray,
+        dividend: npt.NDArray,
+        thresh: int = 0
+        ) -> npt.NDArray:
+     """ Divide two arrays, replacing cases of divide-by-zero (or within some threshold of zero) with zeroes. """
      return np.divide(divisor, dividend, out=np.zeros_like(divisor), where=np.abs(dividend) > thresh)
     
-def debug(msg, start_time=None):
+def debug(
+        msg: str,
+        start_time: float = None
+        ) -> None:
+    """ Debug print statement including details about the call location and (optionally) time elapsed. """
     frame = inspect.stack()[1][0]
     info = inspect.getframeinfo(frame)
     file = path.basename(info.filename)
@@ -60,25 +85,30 @@ def debug(msg, start_time=None):
         msg += ' {:.0f} seconds elapsed.'.format(time() - start_time)
     print(msg)
 
-def coord_mats(shape, res=None, loc=(0.5, 0.5, 0.5), offset=0):
-    if res is None:
-        res = (1,) * len(shape)
-    coord_vecs = (r * (np.arange(s, dtype=float) - int(s * l) + offset) for r, s, l in zip(res, shape, loc))
-    return np.meshgrid(*coord_vecs, indexing='ij')
-
-def masked_copy(arr, mask, fill_val=0):
-    ''' return copy of arr with values outside mask set to fill_val '''
+def masked_copy(
+        arr: npt.NDArray,
+        mask: npt.NDArray[np.bool],
+        fill_val = 0):
+    """ Return copy of array with values outside mask set to the fill value. """
     arr_copy = arr.copy()
     arr_copy[~mask] = fill_val
     return arr_copy
 
-def save_args(args, save_dir):
+def save_args(
+        args: argparse.Namespace,
+        save_dir: str
+        ) -> None:
+    """ Save command-line arguments to a JSON file. """
     args_dict = args.__dict__
     args_dict['cmd'] = " ".join(["python"] + sys.argv)
     with open(path.join(save_dir, 'args.txt'), 'w') as f:
         json.dump(args_dict, f, indent=4)
 
-def list_to_formatted_string(x, format='int'):
+def list_to_formatted_string(
+        x: list,
+        format: str = 'int'
+        ) -> None:
+    """ Convert list to a formatted string. """
     if format == 'int':
         format_list = ['{:.3g}' for _ in x]
     s = ', '.join(format_list)
