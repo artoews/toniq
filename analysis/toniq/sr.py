31d93e7c549c7e5e20e0f058425edf2ce91bd042
Alexander R Toews <artoews@stanford.edu>
2024-05-27 12:53:11 -0700

    refactor

--- analysis/toniq/sr.py
+++ analysis/toniq/sr.py
@@ -11,7 +11,6 @@ from scipy.signal import unit_impulse
 import scipy.ndimage as ndi
 
 from toniq.filter import generic_filter
-from toniq.linop import get_matrix
 from toniq.plot import overlay_mask, colorbar_axis
 from toniq.plot_params import *
 
@@ -46,8 +45,8 @@ def get_map(
     Returns:
         tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]: [map of PSF, map of FWHM in mm]
     """
-    psf = estimate_psf(reference, target, mask, psf_shape, patch_shape, stride, num_workers)
-    fwhm = get_FWHM_from_image(psf, psf_shape, num_workers)
+    psf = map_psf(reference, target, mask, psf_shape, patch_shape, stride, num_workers)
+    fwhm = map_fwhm(psf, psf_shape, num_workers)
     for i in range(fwhm.shape[-1]):
         fwhm[..., i] = fwhm[..., i] * resolution_mm[i]
     if stride == 1:
@@ -55,14 +54,15 @@ def get_map(
         fwhm = sp.resize(fwhm, target.shape[:3] + fwhm.shape[3:])
     return psf, fwhm
 
-def estimate_psf(
+def map_psf(
         image_in: npt.NDArray[np.float64],
         image_out: npt.NDArray[np.float64],
         mask: npt.NDArray[np.bool],
         psf_shape: tuple[int],
         patch_shape: tuple[int],
         stride: int,
-        num_batches: int
+        num_batches: int,
+        batch_axis: int = 2,
         ) -> npt.NDArray[np.float64]:
     """Local PSF estimation for an image pair.
 
@@ -76,44 +76,39 @@ def estimate_psf(
         patch_shape (tuple[int]): patch size used for local PSF estimation
         stride (int): stride between patches
         num_batches (int): parallelization factor
+        batch_axis (int): axis along which to batch patches for parallelization. Defaults to 2.
 
     Returns:
         npt.NDArray[np.float64]: map of the local PSF for each patch
     """
     images_stack = np.stack((image_in, image_out), axis=-1)
     images_stack[~mask, ...] = np.nan
-    batch_axis = 2
-    func = functools.partial(deconvolve_by_model, psf_shape)
+    func = functools.partial(deconvolve, psf_shape)
     return generic_filter(images_stack, func, patch_shape, psf_shape, stride, batch_axis, num_batches=num_batches)
 
-def deconvolve_by_model(
+def deconvolve(
         psf_shape: tuple[int],
-        patch_pair: npt.NDArray[np.float64],
+        image_pair: npt.NDArray[np.float64],
         verbose: bool = False
         ) -> npt.NDArray[np.float64]:
-    """Local PSF estimation for one patch.
+    """Solve PSF deconvolution problem for one pair of input/output image patches.
 
     The inverse problem is solved iteratively by means of Conjugate Gradient.
+    Direct solve takes 4x time using a random matrix as a proxy for A. Constructing A by brute force takes 10x instead of 4x.
 
     Args:
         psf_shape (tuple[int]): PSF footprint
-        patch_pair (npt.NDArray[np.float64]): stack of the two patches from each image in the pair
+        patch_pair (npt.NDArray[np.float64]): stack of the two image patches (stacked in first dimension)
         verbose (bool, optional): whether to show iterative progress bar. Defaults to False.
 
     Returns:
         npt.NDArray[np.float64]: PSF
     """
-    kspace_pair = sp.fft(patch_pair, axes=(0, 1))
-    kspace_in, kspace_out = kspace_pair[..., 0], kspace_pair[..., 1]
-    A = forward_model(kspace_in, psf_shape)
-    # A = forward_model_conv(patch_pair[..., 0], psf_shape)
-    y = sp.resize(patch_pair[..., 1], A.oshape)
-    # A_mat = get_matrix(A)
-    # A_mat = np.random.rand(np.prod(A.oshape), np.prod(A.ishape))
-    # A_inv = np.linalg.pinv(A_mat) # direct solve takes about 4 times longer than iterative solve using a random matrix as a proxy for A. If you call get_matrix and actually construct A by brute force it takes 10x instead of 4x.
-    # soln = np.reshape(A_inv.dot(y.ravel()), psf_shape)
-    app = sp.app.LinearLeastSquares(A, y, x=np.zeros(A.ishape, dtype=np.complex128), tol=1e-10, max_iter=1e10, show_pbar=verbose, lamda=0)
-    # app = sp.app.LinearLeastSquares(A, y, x=np.zeros(A.ishape, dtype=np.float64), tol=1e-10, max_iter=1e10, show_pbar=verbose, lamda=0)
+    x = sp.fft(image_pair[..., 0], axes=(0, 1))
+    A = forward_model(x, psf_shape)
+    y = sp.resize(image_pair[..., 1], A.oshape)
+    x_init = np.zeros(A.ishape, dtype=np.complex128)
+    app = sp.app.LinearLeastSquares(A, y, x=x_init, tol=1e-10, max_iter=1e10, show_pbar=verbose, lamda=0)
     soln = app.run()
     psf = np.abs(soln)
     return psf
@@ -128,18 +123,18 @@ def forward_model(
     Model implements convolution in the DFT domain for computational efficiency.
 
     Args:
-        input_kspace (npt.NDArray[np.float64]): 2D DFT of 3D image patch
+        input_kspace (npt.NDArray[np.float64]): 2D DFT of 3D image patch (last/3rd dimension is image space)
         psf_shape (tuple[int]): PSF footprint; expects 3D PSF with singleton 3rd dimension
 
     Returns:
         sp.linop.Linop: linear operator
     """
-    Z = sp.linop.Resize(image_dft.shape[:2] + (1,), psf_shape)
+    Z = sp.linop.Resize(image_dft.shape[:2] + (1,), psf_shape) # zero padding up to patch size
     F = sp.linop.FFT(Z.oshape, axes=(0, 1))
-    D = sp.linop.Multiply(F.oshape, image_dft)
-    no_wrap_size = tuple(np.array(image_dft.shape[:2]) - np.array(psf_shape[:2]) + np.ones(2, dtype=int))
+    D = sp.linop.Multiply(F.oshape, image_dft) # image convolution implemented in DFT space
     FH = sp.linop.FFT(D.oshape, axes=(0, 1)).H
-    C = sp.linop.Resize(no_wrap_size + image_dft.shape[2:], FH.oshape)
+    no_wrap_size = tuple(np.array(image_dft.shape[:2]) - np.array(psf_shape[:2]) + np.ones(2, dtype=int))
+    C = sp.linop.Resize(no_wrap_size + image_dft.shape[2:], FH.oshape) # cropping to enforce "valid" convolution mode
     return C * FH * D * F * Z
 
 def forward_model_conv(
@@ -160,7 +155,7 @@ def forward_model_conv(
     A = sp.linop.ConvolveFilter(psf_shape, input_image, mode='valid')
     return A
 
-def get_FWHM_from_image(
+def map_fwhm(
         psf: npt.NDArray[np.float64],
         psf_shape: tuple[int],
         num_workers: int,
@@ -179,15 +174,14 @@ def get_FWHM_from_image(
     Returns:
         npt.NDArray[np.float64]: FWHM map
     """
-    func = get_FWHM_from_pixel
     patch_shape = (1, 1, 1)
     out_shape = (sum(p > 1 for p in psf_shape),) # number of non-singleton dimensions of PSF
-    return generic_filter(psf, func, patch_shape, out_shape, stride, batch_axis, num_batches=num_workers)
+    return generic_filter(psf, measure_fwhm, patch_shape, out_shape, stride, batch_axis, num_batches=num_workers)
 
-def get_FWHM_from_pixel(
+def measure_fwhm(
         psf: npt.NDArray[np.float64]
         ) -> list[float]:
-    """Measure the FWHM of a point spread function (PSF).
+    """Measure the FWHM of a multi-dimensional point spread function (PSF).
 
     Args:
         psf (npt.NDArray[np.float64]): PSF
@@ -196,20 +190,17 @@ def get_FWHM_from_pixel(
         list[float]: FWHM in each dimension
     """
     psf = np.abs(np.squeeze(psf))
-    ndim = psf.ndim
-    i_max = np.unravel_index(np.argmax(psf), psf.shape)
+    i_max = tuple(np.unravel_index(np.argmax(psf), psf.shape))
     if psf[i_max] == 0:
-        return (0,) * ndim
+        return (0,) * psf.ndim
     fwhm_list = []
-    for i in range(ndim):
-        slc = list(i_max)
-        slc[i] = slice(None)
-        slc = tuple(slc)
-        fwhm_i = get_FWHM_from_vec(psf[slc], i_max=i_max[i])
+    for i in range(psf.ndim):
+        psf_slc = psf[i_max[:i] + (slice(None),) + i_max[i+1:]]
+        fwhm_i = measure_fwhm_1d(psf_slc, i_max=i_max[i])
         fwhm_list.append(fwhm_i)
     return fwhm_list
 
-def get_FWHM_from_vec(
+def measure_fwhm_1d(
         x: npt.NDArray[np.float64],
         i_max: int = None
         ) -> float:
